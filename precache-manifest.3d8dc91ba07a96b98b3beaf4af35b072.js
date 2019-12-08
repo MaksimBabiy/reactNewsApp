c36f0db318205275dea9524903965a55a8446233
mksninja <kitavaurod@gmail.com>
2019-12-08 18:20:09 +0200

    Updates

---
+++ precache-manifest.3d8dc91ba07a96b98b3beaf4af35b072.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02cc388ed25dccf87d0b24a8c8899492",
+    "url": "/reactNewsApp/index.html"
+  },
+  {
+    "revision": "9ee33faf10f388016e4e",
+    "url": "/reactNewsApp/static/css/2.bd174d61.chunk.css"
+  },
+  {
+    "revision": "f425cf95e75ec2d0eb50",
+    "url": "/reactNewsApp/static/css/main.5b640ed3.chunk.css"
+  },
+  {
+    "revision": "9ee33faf10f388016e4e",
+    "url": "/reactNewsApp/static/js/2.3cce02ad.chunk.js"
+  },
+  {
+    "revision": "f425cf95e75ec2d0eb50",
+    "url": "/reactNewsApp/static/js/main.8b886360.chunk.js"
+  },
+  {
+    "revision": "7e538cb6533b71b53a01",
+    "url": "/reactNewsApp/static/js/runtime-main.dc672e66.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/reactNewsApp/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/reactNewsApp/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/reactNewsApp/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/reactNewsApp/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/reactNewsApp/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/reactNewsApp/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/reactNewsApp/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/reactNewsApp/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/reactNewsApp/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/reactNewsApp/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/reactNewsApp/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/reactNewsApp/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/reactNewsApp/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/reactNewsApp/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/reactNewsApp/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/reactNewsApp/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
